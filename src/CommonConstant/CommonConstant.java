3ecc27ff3f31bc9af87362c2fbc99100f2e9732b
wonderful <wonderful27x@126.com>
2019-08-31 08:37:28 +0800

    wonderful

--- src/CommonConstant/CommonConstant.java
+++ src/CommonConstant/CommonConstant.java
@@ -17,10 +17,10 @@ public class CommonConstant {
    public static final String TABLE_FRIEND_LIST = "friendlist";
    public static final String TABLE_FRIEND_REQUEST = "friendrequest";
    
+   public static final String HOST = "http://172.16.169.56:8080/";
    public static final String IMAGE_PATH = "D:\\VirtualDirectory\\image\\";
    public static final String MESSAGE_PATH = "D:\\VirtualDirectory\\message\\";
    public static final String FRIEND_REQUEST_PATH = "D:\\VirtualDirectory\\friendRequest\\";
-   public static final String HOST = "http://172.16.169.56:8080/";
    
    public static final String IDENTITY_REQUEST = "Who you are ?";
    public static final String ACCEPT = "Socket created !";
